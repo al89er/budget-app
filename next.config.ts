0ab9a4e78b4f6413d6e333b8f6346e2573aac419
dr-afif <muhammadafif@upm.edu.my>
2026-03-24 17:50:41 +0800

    fix: force webpack build for PWA compatibility
    - Update build script to use --webpack
    
    - Add empty turbopack config to silence Next.js 16 default

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,9 @@ const withPWA = require('next-pwa')({
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Silence Turbopack default in Next.js 16 for PWA support
+  // @ts-ignore - Turbopack config is valid but might not be in types yet
+  turbopack: {},
 };
 
 export default withPWA(nextConfig);
